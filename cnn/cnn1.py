8f13ea3ff891a55093728df025af4dba15b7e1cc
Amlana-P <50160346+Amlana-P@users.noreply.github.com>
2020-04-12 14:05:24 +0530

    Add files via upload

--- cnn/cnn1.py
+++ cnn/cnn1.py
@@ -1,31 +1,54 @@
-import keras
-from keras.datasets import mnist
-batch_size = 128
-num_classes = 10
-epochs = 12
+import tensorflow as tf
 
-# input image dimensions
-img_rows, img_cols = 28, 28
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-x_train = x_train.reshape(60000, 28, 28, 1)
-x_test = x_test.reshape(10000, 28, 28, 1)
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
 
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-model = keras.equential()
-model.add(keras.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28,28,1)))
-model.add(keras.Conv2D(64, (3, 3), activation='relu'))
-model.add(keras.MaxPooling2D(pool_size=(2, 2)))
-model.add(keras.Dropout(0.25))
-model.add(keras.Flatten())
-model.add(keras.Dense(128, activation='relu'))
-model.add(keras.Dropout(0.5))
-model.add(keras.Dense(num_classes, activation='softmax'))
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    # The CIFAR labels happen to be arrays,
+    # which is why you need the extra index
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10,
+                    validation_data=(test_images, test_labels))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print(test_acc)
--- cnn/cnn2.py
+++ cnn/cnn2.py
@@ -1,32 +1,53 @@
+import keras
 from keras.datasets import mnist
-import matplotlib.pyplot as plt
-from keras.utils import to_categorical
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import numpy as np
 
-# download mnist data and split into train and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-# plot the first image in the dataset
-plt.imshow(X_train[0])
-# reshape data to fit model
-X_train = X_train.reshape(60000, 28, 28, 1)
-X_test = X_test.reshape(10000, 28, 28, 1)
-# one-hot encode target column
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
-y_train[0]
-# create model
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000,28,28,1)
+x_test = x_test.reshape(10000,28,28,1)
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
 model = Sequential()
-# add model layers
-model.add(Conv2D(64, kernel_size=3, activation = ’relu’, input_shape=(28, 28, 1)))
-model.add(Conv2D(32, kernel_size=3, activation = ’relu’))
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(28,28,1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(10, activation = ’softmax’))
-# compile model using accuracy to measure model performance
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-# train the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3)
-# predict first 4 images in the test set
-model.predict(X_test[:4])
-# actual results for first 4 images in test set
-y_test[:4]
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
